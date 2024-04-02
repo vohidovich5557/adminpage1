4ee50e3e18504c59af8c0c5c8c74cd71099758e8
vohidovich5557 <shokhuk006@gmail.com>
2024-04-02 13:22:32 +0500

    adminpage1

---
+++ src/config/request.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const request = axios.create({ baseURL: "http://localhost:3000/" });
+
+request.interceptors.request.use(
+  (config) => {
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${Cookies.get("user-token")}`,
+    };
+
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+export { request };
